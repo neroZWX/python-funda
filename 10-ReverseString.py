03fdccbf7ebc5ff4da7f14b84d5a7cd9b81f12aa
neroZWX <971370106@qq.com>
2017-10-06 22:04:32 +0100

    ReverseString

--- 10-ReverseString.py
+++ 10-ReverseString.py
@@ -3,5 +3,5 @@ def reverse(): #http://www.jianshu.com/p/c61279736a03 soultion
     return s[::-1]# use this method to reverse string
 print(reverse())# this method principle : This is extended slice syntax.
 # It works by doing [begin:end:step] - by leaving begin and end off and specifying
-#  a step of -1, it reverses a string.
+#  a step of -1, it reverses a string.2
 
